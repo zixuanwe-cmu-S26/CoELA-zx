b13ea4940491c91f6914d0644c1bded3a8a65720
zlxw <wengzixuanzlxw@gmail.com>
2026-06-30 23:29:55 -0400

    need to update model

--- cwah/scripts/symbolic_obs_llm_llm.sh
+++ cwah/scripts/symbolic_obs_llm_llm.sh
@@ -1,11 +1,14 @@
-kill -9 $(lsof -t -i :6314)
+pid=$(lsof -t -i :6314)
+if [ -n "$pid" ]; then
+kill -9 $pid
+fi
 python testing_agents/test_symbolic_LLMs.py \
 --communication \
 --prompt_template_path LLM/prompt_com.csv \
---mode LLMs_comm_gpt-4 \
+--mode LLMs_comm_gpt-5-mini \
 --executable_file ../executable/linux_exec.v2.3.0.x86_64 \
 --base-port 6314 \
---lm_id gpt-4 \
+--lm_id gpt-5-mini \
 --source openai \
 --t 0.7 \
 --max_tokens 256 \
